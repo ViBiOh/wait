f7d813fafdd0cc018a68ac444207305bb8640955
Vincent Boutour <bob@vibioh.fr>
2026-03-13 21:43:10 +0100

    feat: Remove httputils dependency to reduce bin size
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

---
+++ pkg/logger/error.go
@@ -0,0 +1,53 @@
+package logger
+
+import (
+	"errors"
+	"reflect"
+)
+
+type UnwrapJoin interface {
+	Unwrap() []error
+}
+
+type StackTracer interface {
+	StackTrace() string
+}
+
+type errorField struct {
+	Kind    string `json:"kind"`
+	Message string `json:"message"`
+	Stack   string `json:"stack,omitempty"`
+}
+
+func ErrorField(err error) errorField {
+	output := errorField{
+		Kind:    reflect.TypeOf(err).String(),
+		Message: err.Error(),
+	}
+
+	if stacktrace := getStacktrace(err); len(stacktrace) != 0 {
+		output.Stack = stacktrace
+	}
+
+	return output
+}
+
+func getStacktrace(err error) string {
+	errorsToTest := []error{err}
+
+	for index := 0; index < len(errorsToTest); index++ {
+		testedErr := errorsToTest[index]
+
+		if stackTracer, ok := testedErr.(StackTracer); ok {
+			return stackTracer.StackTrace()
+		}
+
+		if joinErr, ok := testedErr.(UnwrapJoin); ok {
+			errorsToTest = append(errorsToTest, joinErr.Unwrap()...)
+		} else if unwraped := errors.Unwrap(testedErr); unwraped != nil {
+			errorsToTest = append(errorsToTest, unwraped)
+		}
+	}
+
+	return ""
+}
---
+++ pkg/logger/logger.go
@@ -0,0 +1,79 @@
+package logger
+
+import (
+	"context"
+	"flag"
+	"io"
+	"log/slog"
+	"os"
+
+	"github.com/ViBiOh/flags"
+)
+
+type Config struct {
+	Level      string
+	TimeKey    string
+	LevelKey   string
+	MessageKey string
+	JSON       bool
+}
+
+func Flags(fs *flag.FlagSet, prefix string, overrides ...flags.Override) *Config {
+	var config Config
+
+	flags.New("Level", "Logger level").Prefix(prefix).DocPrefix("logger").StringVar(fs, &config.Level, "INFO", overrides)
+	flags.New("Json", "Log format as JSON").Prefix(prefix).DocPrefix("logger").BoolVar(fs, &config.JSON, false, overrides)
+	flags.New("TimeKey", "Key for timestamp in JSON").Prefix(prefix).DocPrefix("logger").StringVar(fs, &config.TimeKey, "time", overrides)
+	flags.New("LevelKey", "Key for level in JSON").Prefix(prefix).DocPrefix("logger").StringVar(fs, &config.LevelKey, "level", overrides)
+	flags.New("MessageKey", "Key for message in JSON").Prefix(prefix).DocPrefix("logger").StringVar(fs, &config.MessageKey, "msg", overrides)
+
+	return &config
+}
+
+func init() {
+	slog.SetDefault(configureLogger(os.Stdout, slog.LevelInfo, false, "time", "level", "msg"))
+}
+
+func Init(ctx context.Context, config *Config) {
+	var level slog.Level
+
+	if err := level.UnmarshalText([]byte(config.Level)); err != nil {
+		slog.LogAttrs(ctx, slog.LevelError, err.Error(), slog.String("level", config.Level))
+
+		return
+	}
+
+	slog.SetDefault(configureLogger(os.Stdout, level, config.JSON, config.TimeKey, config.LevelKey, config.MessageKey))
+}
+
+func configureLogger(writer io.Writer, level slog.Level, json bool, timeKey, levelKey, messageKey string) *slog.Logger {
+	options := &slog.HandlerOptions{
+		Level: level,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			switch a.Key {
+			case "msg":
+				a.Key = messageKey
+			case "level":
+				a.Key = levelKey
+			case "time":
+				a.Key = timeKey
+			}
+
+			switch obj := a.Value.Any().(type) {
+			case error:
+				a.Value = slog.AnyValue(ErrorField(obj))
+			}
+
+			return a
+		},
+	}
+
+	var handler slog.Handler
+	if json {
+		handler = slog.NewJSONHandler(writer, options)
+	} else {
+		handler = slog.NewTextHandler(writer, options)
+	}
+
+	return slog.New(handler)
+}
